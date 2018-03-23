4eb861bcf788d67f5e3a60cf56a46a578119b634
Lisa Lee <lisa_lee@alumni.brown.edu>
2018-03-23 17:00:10 -0500

    fixing version

--- lib/recipe_finder.rb
+++ lib/recipe_finder.rb
@@ -1,10 +1,10 @@
 require 'nokogiri'
 require 'open-uri'
 require 'pry'
-require_relative "finder/version.rb"
 require_relative "finder/cli.rb"
 require_relative "finder/scraper.rb"
 require_relative "finder/dish.rb"
+require_relative "finder/version.rb"
 
 module RecipeFinder
 end
